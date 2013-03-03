2d33f9ff5cf6a1ce33b96b2e99a1d2ce7f5f58cd
jleo3 <joseph.leo3@gmail.com>
2013-03-03 17:04:42 -0500

    Issue #3 - Now we can add our exercise, weight, reps, sets, and tweet about it

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
 
   $('.button').click(function() {
+    var exercise = $('#exercise').val();
+    var weight = $('#weight').val();
+    var reps = $('#reps').val();
+    var sets = $('#sets').val();
+    var tweet = "I just completed " + exercise + ":" + weight + "X" + reps + "X" + sets;
+
     $('#twitter-share-button-div').empty()
-
-    // create a clone of the twitter share button template
     var clone = $('.twitter-share-button-template').clone()
-
-    // fix up our clone
     clone.removeAttr("style"); // unhide the clone
-    clone.attr("data-text", "some new text"); 
+    clone.attr("data-text", tweet); 
     clone.attr("class", "twitter-share-button"); 
 
-    // copy cloned button into div that we can clear later
     $('.twitter-share-button-div').append(clone);
 
-    // reload twitter scripts to force them to run, converting a to iframe
     $.getScript("http://platform.twitter.com/widgets.js");
   });
 });
