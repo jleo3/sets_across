0c7b7c46ceab2e96f09f640c303aaee019d79a88
jleo3 <joseph.leo3@gmail.com>
2013-03-16 12:17:48 -0400

    OK, I figured out Jasmine

--- spec/javascripts/home_spec.js
+++ spec/javascripts/home_spec.js
@@ -1,9 +1,5 @@
 describe("Tweet", function() {
   it("contains the twitter handle", function() {
-    function Tweet() {
-      return "@_w";
-    }
-
     expect(Tweet()).toContain("@_weightd");
   });
 });
