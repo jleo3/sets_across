11d9b994081817775dd0f31862a6341845f17482
jleo3 <joseph.leo3@gmail.com>
2013-03-16 20:07:20 -0400

    Add button now populates html table

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -9,6 +9,17 @@ function ClearInput() {
   $('#sets').val('');
 }
 
+function PopulateLiftTable() {
+  var exercise = $('#exercise').val();
+  var weight = $('#weight').val();
+  var reps = $('#reps').val();
+  var sets = $('#sets').val();
+  $('#lift_exercise').text(exercise);
+  $('#lift_weight').text(weight);
+  $('#lift_reps').text(reps);
+  $('#lift_sets').text(sets);
+}
+
 $(document).ready(function() {
   $('.add').click(function() {
     $('.lift').removeAttr("style");
@@ -18,7 +29,9 @@ $(document).ready(function() {
     var sets = $('#sets').val();
     var tweet = "I just finished " + exercise + ": " + weight + "X" + reps + "X" + sets + " using @_weightd! Check it out: weightd.com #Weightd";
 
+    PopulateLiftTable();
     ClearInput();
+
     $('#twitter-share-button-div').empty()
     var clone = $('.twitter-share-button-template').clone()
     clone.removeAttr("style"); // unhide the clone
