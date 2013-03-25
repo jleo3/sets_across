ec1413da9899f576bd915723f50bbb25bf3a1a56
jleo3 <joseph.leo3@gmail.com>
2013-03-25 15:33:59 -0400

    Issue #21: Iterating through all exercises in the table

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -4,13 +4,10 @@ function ComposeTweet() {
   var message = "My Weightd Workout: ";
   $.each(rows, function(index, value) {
     if (index == 0) return true;
-    var exercise = $('#log_exercise').text();
-    var weight = $('#log_weight').text();
-    var reps = $('#log_reps').text();
-    var sets = $('#log_sets').text();
-    message = message + exercise + " " + weight + "X" + reps + "X" + sets
+    var defs = value.getElementsByTagName('td');
+    message = message + defs[0].textContent + " " + defs[1].textContent + "X" + defs[2].textContent + "X" + defs[3].textContent + " ";
   });
-  message = message + " using @_weightd! Check it out: weightd.com #Weightd";
+  message = message + "using @_weightd! Check it out: weightd.com #Weightd";
   return message;
 }
 
@@ -23,30 +20,29 @@ function ClearInput() {
 
 function AppendRow() {
   var lift_log_table = $('#lift_log');
-  var row = "<tr><td id='log_exercise'>" + $('#exercise').val() + "</td>"
-  row = row + "<td id='log_weight'>" + $('#weight').val() + "</td>"
-  row = row + "<td id='log_reps'>" + $('#reps').val() + "</td>"
-  row = row + "<td id='log_sets'>" + $('#sets').val() + "</td></tr>"
+  var row = "<tr><td class='log_exercise'>" + $('#exercise').val() + "</td>"
+  row = row + "<td class='log_weight'>" + $('#weight').val() + "</td>"
+  row = row + "<td class='log_reps'>" + $('#reps').val() + "</td>"
+  row = row + "<td class='log_sets'>" + $('#sets').val() + "</td></tr>"
   lift_log_table.append(row);
 }
 
 $(document).ready(function() {
-  $('.add').click(function() {
-    $('.lift').removeAttr("style");
-    AppendRow();
-    ClearInput();
-
+  $('.share').click(function() {
     if ($('.twitter-share-button-div').children().size() == 0) {
-
       $('#twitter-share-button-div').empty()
       var clone = $('.twitter-share-button-template').clone()
       clone.removeAttr("style");
       clone.attr("data-text", ComposeTweet()); 
       clone.attr("class", "twitter-share-button"); 
-
       $('.twitter-share-button-div').append(clone);
-
       $.getScript("http://platform.twitter.com/widgets.js");
     }
+  });
+
+  $('.add').click(function() {
+    $('.lift').removeAttr("style");
+    AppendRow();
+    ClearInput();
   });
 });
